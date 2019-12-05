1b859c1e85946cbd2c9ff475af475abfa6c3479c
Nicole Liu <nicoleliu_@outlook.com>
2019-12-05 15:15:09 -0500

    Completed guard statements

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Mark: Properties
+    // Connected labels and text views to controller using outlets
+    @IBOutlet weak var questions: UITextField!
+    @IBOutlet weak var studentInput: UITextField!
+    @IBOutlet weak var teacherInput: UITextField!
+    @IBOutlet weak var outputView: UITextView!
+    
+    // Mark: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // Created action for when check answers button is pressed
+    @IBAction func checkAnswers(_ sender: Any) {
+        // Guard statement to check for a number in number of questions
+        guard let questionsString = questions.text, let numberOfQuestions = Int(questionsString) else {
+            outputView.text = "Error. Please enter an integer value greater than 0."
+            return
+        }
+        
+        // Guard statement to check if there is text and it matches number of questions in student answers
+        guard let studentAnswers = studentInput.text, studentAnswers.count == numberOfQuestions else {
+            outputView.text = "Error. Please be sure to enter exactly \(numberOfQuestions) answers."
+            return
+        }
+        
+        // Guard statement to check if there is text and it matches number of questions in teacher answers
+          guard let teacherAnswers = teacherInput.text, teacherAnswers.count == numberOfQuestions else {
+              outputView.text = "Error. Please be sure to enter exactly \(numberOfQuestions) answers."
+              return
+          }
+        
+        
+
+        }
+        
+        
+        
+    }
 
 
-}
 
